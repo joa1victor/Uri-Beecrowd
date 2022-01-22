2d85e6565fc95d9c8c4880f2d83fa94814a4f97c
joa1victor <joa1victor.braga@gmail.com>
2022-01-22 11:12:49 -0300

    Distancia

---
+++ 1016.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+
+        int distancia, tempo, minutos=2, km=1;
+
+    scanf("%d", &distancia);
+    tempo = (minutos*km)*distancia;
+        printf("%d minutos\n", tempo);
+
+    return 0;
+}
