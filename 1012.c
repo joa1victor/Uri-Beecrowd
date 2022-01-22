a3d47b7a2ba367d32df6c2bcfb6af68d9c5212b3
joa1victor <joa1victor.braga@gmail.com>
2022-01-22 11:00:03 -0300

    Area

---
+++ 1012.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+
+    double a, b, c, pi=3.14159, triangulo, circulo, trapezio, quadrado, retangulo;
+
+    scanf("%lf%lf%lf", &a, &b, &c);
+    {
+        triangulo = (a*c)/2;
+    }
+
+    {
+        circulo = pi*(c*c);
+    }
+
+    {
+        trapezio = ((a+b)*c)/2;
+    }
+
+    {
+        quadrado = b*b;
+    }
+
+    {
+        retangulo = a*b;
+    }
+
+    printf("TRIANGULO: %.3lf\nCIRCULO: %.3lf\nTRAPEZIO: %.3lf\nQUADRADO: %.3lf\nRETANGULO: %.3lf\n", triangulo, circulo, trapezio, quadrado, retangulo);
+
+    return 0;
+}
