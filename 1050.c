dbc53b6e70c85bc54fc01a1a1d85bd19932fc977
joa1victor <joa1victor.braga@gmail.com>
2022-01-30 13:47:36 -0300

    DDD

---
+++ 1050.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int ddd, destino;
+
+    scanf("%d", &ddd);
+
+    if (ddd == 61)
+        puts("Brasilia");
+    else
+    if (ddd == 71)
+        puts("Salvador");
+    else
+    if (ddd == 11)
+        puts("Sao Paulo");
+    else
+    if (ddd == 21)
+        puts("Rio de Janeiro");
+    else
+    if (ddd == 32)
+        puts("Juiz de Fora");
+    else
+    if (ddd == 19)
+        puts("Campinas");
+    else
+    if (ddd == 27)
+        puts("Vitoria");
+    else
+    if (ddd == 31)
+        puts("Belo Horizonte");
+    else {
+        puts("DDD nao cadastrado");
+    }
+
+    return 0;
+}
