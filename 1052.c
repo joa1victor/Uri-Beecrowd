fda4b32491a8b02b5709ceccd2c5256ddf980ee4
joa1victor <joa1victor.braga@gmail.com>
2022-01-30 22:49:39 -0300

    Mes

---
+++ 1052.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   int mes;
+
+   scanf("%d", &mes);
+
+   if (mes == 1)
+    puts("January");
+   if (mes == 2)
+    puts("February");
+   if (mes == 3)
+    puts("March");  
+   if (mes == 4)
+    puts("April"); 
+   if (mes == 5)
+    puts("May");
+   if (mes == 6)
+    puts("June");
+   if (mes == 7)
+    puts("July");
+   if (mes == 8)
+    puts("August");   
+   if (mes == 9)
+    puts("September");
+   if (mes == 10)
+    puts("October");
+   if (mes == 11)
+    puts("November");
+   if (mes == 12)
+    puts("December");
+
+    return 0;
+}
