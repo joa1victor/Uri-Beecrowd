8ffee810a2721bc88c999bfda251875c5b713640
joa1victor <joa1victor.braga@gmail.com>
2022-02-05 00:25:08 -0300

    Numeros Impares

---
+++ teste.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int cdia, fdia;
+    int choras, cminutos, csegundos;
+    int fhoras, fminutos, fsegundos;
+    int dh, dm, ds, dd;
+
+
+
+  
+ 
+    return 0;
+}
