76088f30d4560c40af447bf82bb83d1f3998996e
joa1victor <joa1victor.braga@gmail.com>
2022-02-12 20:10:18 -0300

    Resto 2

---
+++ 1075.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int num, i, resto;
+
+    scanf("%d", &num);
+
+    for (i=1; i<=10000; i++){
+
+        if (i%num==2){
+            printf("%d\n", i);
+        }
+    }
+ 
+    return 0;
+}
