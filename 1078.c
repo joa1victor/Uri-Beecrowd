05e12c8ca7ca4edad3ffebc49a11a6e405b92804
joa1victor <joa1victor.braga@gmail.com>
2022-02-12 20:16:28 -0300

    Tabuada

---
+++ 1078.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int num, i, tabu;
+
+    scanf("%d", &num);
+
+    for (i=1; i<=10; i++){
+
+        tabu = i*num;
+
+        printf("%d x %d = %d\n", i, num, tabu);
+
+    }
+ 
+    
+
+    return 0;
+}
