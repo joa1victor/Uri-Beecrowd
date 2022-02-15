17d61e98bd6ec37ddd4deec42a671462af0e4e84
joa1victor <joa1victor.braga@gmail.com>
2022-02-15 14:14:11 -0300

    Sequencia IJ 1

--- 1095.c
+++ 1095.c
@@ -2,26 +2,19 @@
 
 int main(){
 
-    int n, i = -2, j = 65, k, count = 0;
+    int i, j;
 
-    
-    while (j >= 5){
+    i = 1;
+    j = 60;
 
+    while (j >= 0)
+    {
+        printf ("I=%d J=%d\n", i, j);
 
         i += 3;
-
         j -= 5;
-
-        printf("I=%d J=%d\n", i, j);
-
     }
-
-
-
-
-     
-
     
-
+    
     return 0;
 }
