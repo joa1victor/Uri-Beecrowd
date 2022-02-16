8f521807e5164c1754740dccd95e332d19f35651
joa1victor <joa1victor.braga@gmail.com>
2022-02-15 23:00:06 -0300

    Sequencia IJ 3

--- 1097.c
+++ 1097.c
@@ -2,36 +2,17 @@
  
 int main() {
  
-    int i, j;
+    int i = 1;
+    int j = 7;
 
-    for (i=1; i<=9; i+=2){
+    while (i<=9){
 
-        if (i==1){
-            for (j=7; j>=5; j--){
-                printf("I=%d J=%d\n", i, j);
-            }
-        }
-        if (i==3){
-            for (j=9; j>=7; j--){
-                printf("I=%d J=%d\n", i, j);
-            }
-        }
-        if (i==5){
-            for (j=11; j>=9; j--){
-                printf("I=%d J=%d\n", i, j);
-            }
-        }
-        if (i==7){
-            for (j=13; j>=11; j--){
-                printf("I=%d J=%d\n", i, j);
-            }
-        }
-        if (i==9){
-            for (j=15; j>=13; j--){
-                printf("I=%d J=%d\n", i, j);
-            }
-        }
+        printf ("I=%d J=%d\n", i, j--);
+        printf ("I=%d J=%d\n", i, j--);
+        printf ("I=%d J=%d\n", i, j--);
 
+        i += 2;
+        j += 5;
     }
  
     return 0;
