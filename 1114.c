d7c10fe3238e59a889157105619c614a98c65c07
joa1victor <joa1victor.braga@gmail.com>
2022-03-25 02:28:02 -0300

    Senha Fixa

---
+++ 1114.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    int i;
+    int senha;
+
+    while(senha!=2002){
+
+        scanf("%d", &senha);
+
+        if (senha!=2002){
+            printf("Senha Invalida\n");
+        } else {
+            puts("Acesso Permitido");
+        }        
+    i++;
+    }
+
+    return 0;
+}
